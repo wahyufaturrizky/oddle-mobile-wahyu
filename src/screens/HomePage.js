399eb54b61ca6e3b5af7db2e3bb9eeee38bccd9e
WAHYU FATUR RIZKI <wahyufaturrizkyy@gmail.com>
2022-06-06 09:14:47 +0700

    feat: add feature background gradient

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,15 +1,47 @@
 import React, {useEffect} from 'react';
-import {View} from 'react-native';
+import {FlatList, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import {ButtonVariant} from 'demo-component/ButtonVariant';
+import {BasicLayout} from 'layout/BasicLayout';
+import {BackgroundImageLayout} from 'layout/BackgroundImageLayout';
+import {PaddingEnum, MarginEnum, BorderRadiusEnum} from 'styles/Spacer';
 
 const HomePage = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <View>
-      <ButtonVariant />
+    <BackgroundImageLayout source={require('assets/background-gradient.jpg')}>
+      <BasicLayout>
+        <FlatList
+          data={[1]}
+          keyExtractor={({id}) => id?.toString()}
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          contentContainerStyle={{
+            paddingBottom: PaddingEnum['4x'],
+          }}
+          ItemSeparatorComponent={() => (
+            <View style={{marginVertical: MarginEnum['0.5x']}} />
+          )}
+          renderItem={({item, index}) => (
+            <CardRecomended {...item} key={index} onPress={() => {}} />
+          )}
+        />
+      </BasicLayout>
+    </BackgroundImageLayout>
+  );
+};
+
+const CardRecomended = (props) => {
+  return (
+    <View
+      style={{
+        backgroundColor: '#F5F5F5',
+        borderRadius: BorderRadiusEnum['2x'],
+        height: 372,
+        padding: 10,
+      }}>
+      <View></View>
     </View>
   );
 };
